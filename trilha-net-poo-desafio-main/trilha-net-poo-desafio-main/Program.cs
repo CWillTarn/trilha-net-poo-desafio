cbc24cfe0158ce294009fde289708a8fc1109b3a
CWillTarn <camilacmwill@gmail.com>
2024-01-08 16:09:54 -0300

    commit

---
+++ trilha-net-poo-desafio-main/trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatzapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 3", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
